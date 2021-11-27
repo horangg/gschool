7c53e03b6c67864ff23fd67027258a138bdc811c
horangg <94329609+horangg@users.noreply.github.com>
2021-11-28 00:35:20 +0900

    스크롤-TOP버튼
    누르면 페이지 맨 위로 이동하는 버튼 만들었다..!
    position: pixed 사용해서 버튼 만들어 봄
    버튼에 css랑 js 다 입혀봐서 신기햇다. 근데 JS 코드는 좀 더 이해가 필요할 듯

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// 버튼 클릭 시 "이동하시겠습니까?" 띄우기
 function button_event() {
   var result = confirm('이동하시겠습니까?');
   if (result) {
@@ -6,3 +7,36 @@ function button_event() {
   } else {
   }
 }
+
+function home() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// 스크롤-TOP버튼
+scrollTop('top_button', 150);
+// 두 번째 파라미터: 스크롤 속도. 150ms로 지정됨
+function scrollTop(elem, duration) {
+  let target = document.getElementById(elem);
+
+  target.addEventListener('click', function () {
+    let currentY = window.pageYOffset;
+    let step = duration / currentY > 1 ? 10 : 100;
+    // 한 번에 움직이는 스크롤 양
+    let timeStep = duration / currentY * step;
+    let intervalID = setInterval(scrollUp, timeStep);
+
+    function scrollUp() {
+      currentY = window.pageYOffset;
+      if (currentY === 0) {
+        clearInterval(intervalID);
+      } else {
+        scrollBy(0, -step);
+      }
+    }
+  });
+}
+
